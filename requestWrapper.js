868c8f5d6c2122795fe2c914e0ff3f8b606a00aa
Thinh <ltthinhdev@gmail.com>
2019-06-13 17:54:21 +0800

    Call reduce request function

--- requestWrapper.js
+++ requestWrapper.js
@@ -1,23 +1,43 @@
+const DELAY_TIME = 500;
+
 module.exports = (() => {
     let pendingProcess;
 
-    return { 
-        callRequest: (request, successFunc) => {
-            pendingProcess = successFunc;
-            request.then(() => {
-                if(pendingProcess) {
-                    pendingProcess();
-                    pendingProcess = null;
+    let pendingRequest;
+    let prevRequestTime = (new Date()).getTime();
+
+    let callRequest = (request, successFunc) => {
+        pendingProcess = successFunc;
+        request.then(() => {
+            if(pendingProcess) {
+                pendingProcess();
+                pendingProcess = null;
+            }
+        })
+    }
+
+    let callReduceRequest = (requestFunc, successFunc) => {
+        pendingProcess = successFunc;
+        pendingRequest = requestFunc;
+        prevRequestTime = (new Date()).getTime();
+        setTimeout(() => {
+            if(pendingRequest) {
+                let nowTime = (new Date()).getTime();
+                if(nowTime - prevRequestTime >= DELAY_TIME) {
+                    pendingRequest().then(() => {
+                        if(pendingProcess) {
+                            pendingProcess();
+                            pendingProcess = null;
+                        }
+                    });
+                    pendingRequest = null;
                 }
-            })
-        }
+            }
+        }, DELAY_TIME);
+    }
+
+    return { 
+        callRequest: callRequest,
+        callReduceRequest: callReduceRequest
     }
 })()
-
-//        Hướng dẫn sử dụng:
-
-//        import RequestWraper from '....';
-
-//        //request là hàm gọi api (phần phía trước "then")
-//        //successFunc là hàm sử lý sau khi api callback (phần bên trong "then")
-//        RequestWraper.callRequest(request, successFunc);
